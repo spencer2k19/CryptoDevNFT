a16d70e4454729851cf038227c2152e7351bc3d3
spencer2k19 <hachemeloic@gmail.com>
2022-02-10 16:27:25 +0100

    Finalized smart contract with hardhat

---
+++ constants/index.js
@@ -0,0 +1,5 @@
+//Address of the Whitelist Contract that is deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xF244f8C19aDa7bD9F45a887FE310f4129F337da3";
+//url to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://dapp-nft.vercel.app/api/";
+module.exports = {WHITELIST_CONTRACT_ADDRESS,METADATA_URL};
